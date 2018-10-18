be1d9b1f252170ffe154a06d5c84669e98ceb69b
smlee-hdactech <sm.lee4573@hdactech.com>
2018-10-18 15:17:54 +0900

    build for debugging with "debug" option

--- build.sh
+++ build.sh
@@ -6,7 +6,12 @@ core=`cat /proc/cpuinfo | grep cores | wc -l`
 cd depends
 make HOST=x86_64-unknown-linux-gnu -j$core
 cd ..
-./configure --prefix=`pwd`/depends/x86_64-unknown-linux-gnu --enable-cxx --enable-shared --enable-static --with-pic
+if [[ $1 =~ "debug" ]]
+then
+        debug_opt="--enable-debug"
+fi
+#echo opt=$debug_opt
+#exit 0
+./configure --prefix=`pwd`/depends/x86_64-unknown-linux-gnu --enable-cxx --enable-shared --enable-static --with-pic $debug_opt
 make -j $core
-
 
